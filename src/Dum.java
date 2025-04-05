550bed0cbaa84a53b46b575cce2a4e2716d0d48e
Bodikk <bohdanator@seznam.cz>
2025-04-05 16:17:22 +0200

    zkouska domečku

--- src/Dum.java
+++ src/Dum.java
@@ -13,24 +13,24 @@ public class Dum {
         int houseHeight = size;
         int roofHeight = size / 2;
 
-        // Definice bodů pro střechu
+
         int[] xPoints = {x, x + size / 2, x + size};
         int[] yPoints = {y + roofHeight, y, y + roofHeight};
 
         Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(3)); // Silnější obrys
+        g2.setStroke(new BasicStroke(3));
 
-        // Kreslení střechy
-        g2.setColor(Color.WHITE);
-        g2.fillPolygon(xPoints, yPoints, 3); // Vyplnění bílou
-        g2.setColor(Color.BLACK);
-        g2.drawPolygon(xPoints, yPoints, 3); // Černý obrys
 
-        // Kreslení domu
         g2.setColor(Color.WHITE);
-        g2.fillRect(x, y + roofHeight, size, houseHeight); // Vyplnění bílou
+        g2.fillPolygon(xPoints, yPoints, 3);
         g2.setColor(Color.BLACK);
-        g2.drawRect(x, y + roofHeight, size, houseHeight); // Černý obrys
+        g2.drawPolygon(xPoints, yPoints, 3);
+
+
+        g2.setColor(Color.WHITE);
+        g2.fillRect(x, y + roofHeight, size, houseHeight);
+        g2.setColor(Color.BLACK);
+        g2.drawRect(x, y + roofHeight, size, houseHeight);
     }
 
 
